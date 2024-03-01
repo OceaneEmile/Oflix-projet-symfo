ee8ceca7f5ff199c91755b09fb25e7f4ccbb010d
OceaneEmile <oceane.emile@oclock.school>
2024-03-01 11:09:38 +0100

    dynamisation genre homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GenreRepository;
 use App\Repository\MovieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,11 +11,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home-page')]
-    public function home(MovieRepository $movieRepository): Response
+    public function home(MovieRepository $movieRepository,GenreRepository $genreRepository): Response
     {
         $movie = $movieRepository->findAll();
+        $genres = $genreRepository->findAll();
         return $this->render('main/main.html.twig', [
-            'movie' => $movie
+            'movie' => $movie,
+            'genres' => $genres
         ]);
     }
 
