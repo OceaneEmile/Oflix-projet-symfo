cec8d62bda242df1f4eced55dba072f927053083
OceaneEmile <oceane.emile@oclock.school>
2024-03-02 13:08:15 +0100

    creation Form Review

---
+++ src/Form/ReviewType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use App\Entity\Review;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('content')
+            ->add('rating')
+            ->add('reactions')
+            ->add('watchedAt')
+            ->add('movie', EntityType::class, [
+                'class' => Movie::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
