bcac79ab0cdc18d572a1072565e86d43474c9643
OceaneEmile <oceane.emile@oclock.school>
2024-03-02 14:47:26 +0100

    creation form review/id/add

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Movie;
 use App\Entity\Review;
 use App\Form\ReviewType;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,12 +17,17 @@ class ReviewController extends AbstractController
      *
      * @return Response
      */
+     /**
+     * Ajout d'une critique sur un film
+     *
+     * @return Response
+     */
     #[Route('/review/{id}/add', name: 'app_review_add')]
-    public function add(Review $review): Response
+    public function add(Movie $movie): Response
     {
-        
+        // On créer l'instance de Review
         $review = new Review();
-        // Form creation
+        // On construit le formulaire qui va construire notre $objet $review
         $form = $this->createForm(ReviewType::class, $review);
         return $this->render('review/add.html.twig', [
             'form' => $form,
